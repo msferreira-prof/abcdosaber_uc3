e6a2775957df635fcdabdc9340cd644d4f72305e
Marcos da Silva Ferreira <msferreira.prof@gmail.com>
2025-02-17 21:55:37 -0300

    criacao template

--- abcdosaber/urls.py
+++ abcdosaber/urls.py
@@ -17,9 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tipodeatividade/', include('tipodeatividade.urls')),
     path('instrutor/', include('instrutor.urls')),
     path('aluno/', include('aluno.urls')),
+    path('turma/', include('turma.urls')),
+    path('titulo/', include('titulo.urls')),
 ]
+
+urlpatterns += [
+     path('', RedirectView.as_view(url='turma/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- turma/views.py
+++ turma/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 from turma.models import Turma
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'escola.html')
+
 def listar(request):
     lista_turma = Turma.objects.all()
     return HttpResponse(lista_turma)
