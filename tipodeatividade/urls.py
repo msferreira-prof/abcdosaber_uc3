63a65f5d577180bd486afa6ded858f3a9c35786f
Marcos da Silva Ferreira <msferreira.prof@gmail.com>
2025-02-21 21:46:19 -0300

    lista tipo de atividade

--- tipodeatividade/urls.py
+++ tipodeatividade/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'tipodeatividade'
+
 urlpatterns = [
-    path('indice', views.index, name='index'),
     path('lista/', views.listar, name='listar'),
-    path('bomdia/', views.show_mensagem, name='bomdia'),
     path('<int:ta_codigo>/', views.detalhe_tipodeatividade, name='tipodeatividade')
 ]
--- tipodeatividade/views.py
+++ tipodeatividade/views.py
@@ -4,26 +4,15 @@ from django.http import HttpResponse
 from tipodeatividade.models import TipoDeAtividade
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<!DOCType='html'><html><body><p>Olá, estou no App 'Tipo de Atividade'</p></body></html>")
-
-
 def listar(request):
-    lista_tipodeatividade = TipoDeAtividade.objects.order_by("descricao")    
-    resposta = "<ul>"
-    for tipodeatividade in lista_tipodeatividade:
-        resposta += '<li>{0}</li>'.format(tipodeatividade.descricao)
+    lista_tipodeatividade = TipoDeAtividade.objects.all()
+    contexto = {
+        'tiposdeatividade': lista_tipodeatividade
+    }
     
-    resposta += '</ul>'
-    print(resposta)
-    return HttpResponse(resposta)
+    return render(request, 'tipodeatividade/listarTipoDeAtividade.html', context=contexto)
 
-    
-def show_mensagem(request):
-    x = "M"
-    nome = x + "arcos, tudo bem?"
-    return HttpResponse(f"Bom dia!{nome}")   
-
+   
 def detalhe_tipodeatividade(request, ta_codigo):
     tipodeatividade = TipoDeAtividade.objects.get(pk=ta_codigo)
     return HttpResponse(tipodeatividade)
